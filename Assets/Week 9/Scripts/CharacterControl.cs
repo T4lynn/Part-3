fa87a4c7ca88d016c79c27b383b01e9dfa4af83a
pappinm <marin@pappin.ca>
2024-03-13 17:36:27 -0400

    finished task 4b

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMP_Text charactertext;
+    public static string name1 = "none";
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -14,6 +18,15 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        name1 = villager.name;
     }
-    
+    private void Start()
+    {
+
+        name1 = "None";
+    }
+    private void Update()
+    {
+        charactertext.text = name1;
+    }
 }
